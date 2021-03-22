e9e20cb76bbf0d840a29e94f1fdcf6f238536a93
wajih <bouali.wajih@gmail.com>
2021-03-23 00:04:30 +0100

    checkpoint state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import robot from "./robot.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      person: {
+        fullName: "Wajih Bou Ali",
+        imgSRC: robot,
+        bio: "This is me",
+        profession: "Student",
+      },
+      show: false,
+      time: 0,
+    };
+    this.handleShow = () => {
+      this.setState({ show: !this.state.show });
+    };
+  }
+  componentDidMount() {
+    setInterval(() => {
+      this.setState(() =>
+        this.state.show ? { time: this.state.time + 1 } : 0
+      );
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.handleShow} className="but">
+          {" "}
+          SHOW ME
+        </button>
+        {this.state.show ? (
+          <div>
+            <h1
+              style={{
+                fontStyle: "bold",
+                color: "brown",
+                textAlign: "center",
+              }}
+            >
+              {this.state.person.fullName}
+            </h1>
+            <img
+              src={this.state.person.imgSRC}
+              alt="pic"
+              style={{
+                fontStyle: "bold",
+                color: "aqua",
+                width: "300px",
+                height: "300px",
+                borderRadius: "20px",
+                textAlign: "center",
+              }}
+            />
+            <h1
+              style={{
+                fontStyle: "bold",
+                color: "brown",
+                textAlign: "center",
+              }}
+            >
+              {this.state.person.bio}
+            </h1>
+            <h1
+              style={{
+                fontStyle: "bold",
+                color: "brown",
+                textAlign: "center",
+              }}
+            >
+              {this.state.person.profession}
+            </h1>
+          </div>
+        ) : null}
+        <h3
+          style={{
+            fontStyle: "bold",
+            color: "red",
+            textAlign: "center",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Timing:{this.state.time}
+        </h3>
+      </div>
+    );
+  }
 }
 
 export default App;
